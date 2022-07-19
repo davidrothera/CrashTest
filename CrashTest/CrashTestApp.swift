9bf24398abcc5c76c6f4c0895fd159a99aa0aec1
David Rothera <david.rothera@gmail.com>
2022-07-19 21:26:32 +0100

    Initial commit

---
+++ CrashTest/CrashTestApp.swift
@@ -0,0 +1,18 @@
+//
+//  CrashTestApp.swift
+//  CrashTest
+//
+//  Created by David Rothera on 19/07/2022.
+//
+
+import SwiftUI
+import Sentry
+
+@main
+struct CrashTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
